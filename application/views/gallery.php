5a81254414e1418382c7faedbec8032d8c17b6f3
ktaaam <kevint4m@gmail.com>
2017-01-24 17:57:59 -0800

    Fixed the file pathing for the files ( about, gallery, welcome)

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -14,9 +14,9 @@
                 <span class="myhead">Simple Image Gallery</span>
                 <span class="mynav">
                     <ul>
-                        <li><a href="/welcome.php">Home</a></li>
-                        <li><a href="/gallery.php">Gallery</a></li>
-                        <li><a href="/about.php">About</a></li>
+                        <li><a href="/welcome">Home</a></li>
+                        <li><a href="/">Gallery</a></li>
+                        <li><a href="/about">About</a></li>
                     </ul>
                 </span>
             </div>
