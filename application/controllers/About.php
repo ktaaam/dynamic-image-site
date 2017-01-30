24135f7fc4362d42daed428a7950c08fd41259e9
alexbai94 <alexbai94@gmail.com>
2017-01-29 16:52:21 -0800

    Revert "Added database functionality"

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,4 +1,3 @@
-
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
@@ -25,31 +24,3 @@ class About extends Application
 	}
 
 }
-=======
-<?php
-
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class About extends Application
-{
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/
-	 * 	- or -
-	 * 		http://example.com/welcome/index
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->data['pagebody'] = 'about';
-		$this->render();
-	}
-
-}
-
